c1d0b25c28f7d621b2adbd8ef2d4f0d7523e10a6
Yisi Liu <85250232+yisliu@users.noreply.github.com>
2023-01-05 06:48:58 +0000

    new

--- .ccls-cache/@home@runner@agecpp/main.cpp
+++ .ccls-cache/@home@runner@agecpp/main.cpp
@@ -3,10 +3,15 @@ using namespace std;
 
 long long f(int n){
   //prevent 0
-  if(n<=2){
+  if(n==1){
+    return 0;
+  }
+  if(n==2){
     return 1;
   }
+  //4 = 15
   //arrangement methods
+  
   return (n-1)*(f(n-1)+f(n-2));
 }
 
